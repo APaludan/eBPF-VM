f6f67c0c096682e8fa8ca99b677125ba2cb04402
andreas <andreashp@outlook.com>
2026-03-31 19:27:04 +0200

    fix compiler warnings

--- src/vm_instructions/vm_inst.cpp
+++ src/vm_instructions/vm_inst.cpp
@@ -19,15 +19,15 @@ std::unordered_map<int, std::vector<vm_inst>> generate_programs(pid_t protected_
 std::vector<vm_inst> make_ptrace_program(pid_t protected_pid)
 {
     return {
-        vm_inst{OP_LOAD, 1, 0, protected_pid},      // 00) r1 = protected_pid
-        vm_inst{OP_CALL, 0, 0, 14},                 // 01) call bpf_get_current_pid_tgid (nr 14)
-        vm_inst{OP_RSHIFT, 0, 0, 32},               // 02) = r0 >> 32 (extract PID only)
+        vm_inst{OP_LOAD, 1, 0, protected_pid, 0},      // 00) r1 = protected_pid
+        vm_inst{OP_CALL, 0, 0, 14, 0},                 // 01) call bpf_get_current_pid_tgid (nr 14)
+        vm_inst{OP_RSHIFT, 0, 0, 32, 0},               // 02) = r0 >> 32 (extract PID only)
         vm_inst{OP_READ_CTX, 2, 0, sizeof(pid_t), 24}, // 03) read the target pid from ctx + offset 24 = (ctx->args[1])
 
-        vm_inst{OP_JNEQ, 1, 2, 2},    // 04) if r1(protected pid) != r2(target pid): jump to exit (pc +2)
-        vm_inst{OP_RINGBUF, 0, 0, 0}, // 05) submit info to ringbuf
-        vm_inst{OP_LOAD, 0, 0, 0},    // 06) set exit code
-        vm_inst{OP_EXIT, 0, 0, 0}     // 07) exit
+        vm_inst{OP_JNEQ, 1, 2, 2, 0},    // 04) if r1(protected pid) != r2(target pid): jump to exit (pc +2)
+        vm_inst{OP_RINGBUF, 0, 0, 0, 0}, // 05) submit info to ringbuf
+        vm_inst{OP_LOAD, 0, 0, 0, 0},    // 06) set exit code
+        vm_inst{OP_EXIT, 0, 0, 0, 0}     // 07) exit
     };
 };
 
@@ -44,27 +44,27 @@ std::vector<vm_inst> make_lsm_bpf_program()
     const int cmd_link_detach = 34;
 
     return {
-        //vm_inst{OP_EXIT, 0, 0, 0},
-        vm_inst{OP_LOAD, 1, 0, pid},
-        vm_inst{OP_CALL, 0, 0, 14},   // bpf_get_current_pid_tgid
-        vm_inst{OP_RSHIFT, 0, 0, 32}, // r0 = pid
-        vm_inst{OP_JNEQ, 1, 0, 3},
-        vm_inst{OP_LOAD, 0, 0, 0},
-        vm_inst{OP_EXIT, 0, 0, 0},
+        //vm_inst{OP_EXIT, 0, 0, 0, 0},
+        vm_inst{OP_LOAD, 1, 0, pid, 0},
+        vm_inst{OP_CALL, 0, 0, 14, 0},   // bpf_get_current_pid_tgid
+        vm_inst{OP_RSHIFT, 0, 0, 32, 0}, // r0 = pid
+        vm_inst{OP_JNEQ, 1, 0, 3, 0},
+        vm_inst{OP_LOAD, 0, 0, 0, 0},
+        vm_inst{OP_EXIT, 0, 0, 0, 0},
 
-        vm_inst{OP_LOAD, 1, 0, cmd_prog_get_next_id},
-        vm_inst{OP_LOAD, 2, 0, cmd_link_get_next_id},
-        vm_inst{OP_LOAD, 3, 0, cmd_map_get_next_id},
-        vm_inst{OP_LOAD, 4, 0, cmd_link_detach},
-        vm_inst{OP_READ_CTX, 8, 0, sizeof(int)}, // read command to r8
-        vm_inst{OP_JEQ, 4, 8, 5},                // if r8 == r1 ||
-        vm_inst{OP_JEQ, 1, 8, 4},                // r8 == r1 ||
-        vm_inst{OP_JEQ, 2, 8, 3},                // r8 == r2 ||
-        vm_inst{OP_JEQ, 3, 8, 2},                // r8 == r3
-        vm_inst{OP_JMP, 0, 0, 3},
-        vm_inst{OP_RINGBUF, 0, 0, 0},
-        vm_inst{OP_LOAD, 0, 0, -EPERM}, // return -EPERM to block operation
-        vm_inst{OP_EXIT, 0, 0, 0},
+        vm_inst{OP_LOAD, 1, 0, cmd_prog_get_next_id, 0},
+        vm_inst{OP_LOAD, 2, 0, cmd_link_get_next_id, 0},
+        vm_inst{OP_LOAD, 3, 0, cmd_map_get_next_id, 0},
+        vm_inst{OP_LOAD, 4, 0, cmd_link_detach, 0},
+        vm_inst{OP_READ_CTX, 8, 0, sizeof(int), 0}, // read command to r8
+        vm_inst{OP_JEQ, 4, 8, 5, 0},                // if r8 == r1 ||
+        vm_inst{OP_JEQ, 1, 8, 4, 0},                // r8 == r1 ||
+        vm_inst{OP_JEQ, 2, 8, 3, 0},                // r8 == r2 ||
+        vm_inst{OP_JEQ, 3, 8, 2, 0},                // r8 == r3
+        vm_inst{OP_JMP, 0, 0, 3, 0},
+        vm_inst{OP_RINGBUF, 0, 0, 0, 0},
+        vm_inst{OP_LOAD, 0, 0, -EPERM, 0}, // return -EPERM to block operation
+        vm_inst{OP_EXIT, 0, 0, 0, 0},
     };
 }
 
@@ -80,46 +80,46 @@ std::vector<vm_inst> make_lsm_open_program(pid_t protected_pid)
 
     return {
         // vm_inst(op, dst, src, val)
-        vm_inst{OP_LOAD, 1, 0, protected_pid}, // 01) r1 = protected_pid, 0
-        vm_inst{OP_CALL, 0, 0, 14},            // 02) bpf_get_current_pid_tgid, 1
-        vm_inst{OP_RSHIFT, 0, 0, 32},          // 03) r0 = pid, 2
-        vm_inst{OP_JNEQ, 1, 0, 2},             // 04) ,3
-        vm_inst{OP_EXIT, 0, 0, 0},             // 05) early exit if call from protected pid, 4
+        vm_inst{OP_LOAD, 1, 0, protected_pid, 0}, // 01) r1 = protected_pid, 0
+        vm_inst{OP_CALL, 0, 0, 14, 0},            // 02) bpf_get_current_pid_tgid, 1
+        vm_inst{OP_RSHIFT, 0, 0, 32, 0},          // 03) r0 = pid, 2
+        vm_inst{OP_JNEQ, 1, 0, 2, 0},             // 04) ,3
+        vm_inst{OP_EXIT, 0, 0, 0, 0},             // 05) early exit if call from protected pid, 4
 
         vm_inst{OP_READ_CTX, 3, 0, sizeof(void *), 32},   // 06) r3 = *inode, 5
-        vm_inst{OP_ADD, 3, 0, 40},                     // 07) r3 += 40 (offset), 6
-        vm_inst{OP_READ, 4, 3, sizeof(void *)},        // 08) r4 = *inode->i_sb, 7
-        vm_inst{OP_ADD, 4, 0, 96},                     // 09) r4 += 96 (offset), 8
-        vm_inst{OP_READ, 5, 4, sizeof(unsigned long)}, // 10) r5 = *i_sb->s_magic, 9
-        vm_inst{OP_LOAD, 6, 0, proc_super_magic_num},  // 11) r6 = procfs magic num, 10
-        vm_inst{OP_JEQ, 5, 6, 2},                      // 12) ,11
-        vm_inst{OP_EXIT, 0, 0, 0},                     // 13) exit if not procfs, 12
+        vm_inst{OP_ADD, 3, 0, 40, 0},                     // 07) r3 += 40 (offset), 6
+        vm_inst{OP_READ, 4, 3, sizeof(void *), 0},        // 08) r4 = *inode->i_sb, 7
+        vm_inst{OP_ADD, 4, 0, 96, 0},                     // 09) r4 += 96 (offset), 8
+        vm_inst{OP_READ, 5, 4, sizeof(unsigned long), 0}, // 10) r5 = *i_sb->s_magic, 9
+        vm_inst{OP_LOAD, 6, 0, proc_super_magic_num, 0},  // 11) r6 = procfs magic num, 10
+        vm_inst{OP_JEQ, 5, 6, 2, 0},                      // 12) ,11
+        vm_inst{OP_EXIT, 0, 0, 0, 0},                     // 13) exit if not procfs, 12
 
         vm_inst{OP_READ_CTX, 3, 0, sizeof(void *), 32}, // 14) r3 = *inode, 15
-        vm_inst{OP_SUB, 3, 0, 72},                   // 15) r3 = *proc_inode, 16
-        vm_inst{OP_READ, 4, 3, sizeof(void *)},      // 16) r4 = *struct pid, 17
-        vm_inst{OP_ADD, 4, 0, 144},                  // 17) r4 = *upid[0], 18
-        vm_inst{OP_READ, 5, 4, sizeof(int)},         // 18) r5 = target pid, 19
-        vm_inst{OP_JNEQ, 0, 8, 3},                   // 19) exit if read failed, means it is probably not procfs anyway idk
-        vm_inst{OP_JEQ, 5, 1, 3},                    // 20) jump if (r5 == r1), 21
-        vm_inst{OP_LOAD, 0, 0, 0},                   // 21) set return val to 0
-        vm_inst{OP_EXIT, 0, 0, 0},                   // 22) exit if not protected pid, 23
+        vm_inst{OP_SUB, 3, 0, 72, 0},                   // 15) r3 = *proc_inode, 16
+        vm_inst{OP_READ, 4, 3, sizeof(void *), 0},      // 16) r4 = *struct pid, 17
+        vm_inst{OP_ADD, 4, 0, 144, 0},                  // 17) r4 = *upid[0], 18
+        vm_inst{OP_READ, 5, 4, sizeof(int), 0},         // 18) r5 = target pid, 19
+        vm_inst{OP_JNEQ, 0, 8, 3, 0},                   // 19) exit if read failed, means it is probably not procfs anyway idk
+        vm_inst{OP_JEQ, 5, 1, 3, 0},                    // 20) jump if (r5 == r1), 21
+        vm_inst{OP_LOAD, 0, 0, 0, 0},                   // 21) set return val to 0
+        vm_inst{OP_EXIT, 0, 0, 0, 0},                   // 22) exit if not protected pid, 23
 
         vm_inst{OP_READ_CTX, 3, 0, sizeof(void *), 64 + 8}, // 23) r3 = *dentry
-        vm_inst{OP_ADD, 3, 0, 32 + 8},                   // 24) r3 = **name
-        vm_inst{OP_READ, 3, 3, sizeof(void *)},          // 25) r3 = *name
-        vm_inst{OP_READ, 5, 3, sizeof(void *)},          // 26) r3 = first 8 bytes of name. should probably be on stack and have variable size
-        vm_inst{OP_LOAD, 4, 0, (long long)maps},         // 27) 1: load sus filename
-        vm_inst{OP_JEQ, 5, 4, 6},                        // 28) 2: if sus jump to submit ringbuf
-        vm_inst{OP_LOAD, 4, 0, (long long)smaps},        // 29) 1
-        vm_inst{OP_JEQ, 5, 4, 4},                        // 30) 2
-        vm_inst{OP_LOAD, 4, 0, (long long)mem},          // 31) 1
-        vm_inst{OP_JEQ, 5, 4, 2},                        // 32) 2
-        vm_inst{OP_JMP, 0, 0, 4},                        // 33) jump over ringbuf submit if not sus
-        vm_inst{OP_RINGBUF, 0, 0, 0},                    //
-        vm_inst{OP_PRINTS, 0, 3, 0},                     // 34) print sus file name
+        vm_inst{OP_ADD, 3, 0, 32 + 8, 0},                   // 24) r3 = **name
+        vm_inst{OP_READ, 3, 3, sizeof(void *), 0},          // 25) r3 = *name
+        vm_inst{OP_READ, 5, 3, sizeof(void *), 0},          // 26) r3 = first 8 bytes of name. should probably be on stack and have variable size
+        vm_inst{OP_LOAD, 4, 0, (long long)maps, 0},         // 27) 1: load sus filename
+        vm_inst{OP_JEQ, 5, 4, 6, 0},                        // 28) 2: if sus jump to submit ringbuf
+        vm_inst{OP_LOAD, 4, 0, (long long)smaps, 0},        // 29) 1
+        vm_inst{OP_JEQ, 5, 4, 4, 0},                        // 30) 2
+        vm_inst{OP_LOAD, 4, 0, (long long)mem, 0},          // 31) 1
+        vm_inst{OP_JEQ, 5, 4, 2, 0},                        // 32) 2
+        vm_inst{OP_JMP, 0, 0, 4, 0},                        // 33) jump over ringbuf submit if not sus
+        vm_inst{OP_RINGBUF, 0, 0, 0, 0},                    //
+        vm_inst{OP_PRINTS, 0, 3, 0, 0},                     // 34) print sus file name
 
-        vm_inst{OP_LOAD, 0, 0, 0}, // 35) set return val
-        vm_inst{OP_EXIT, 0, 0, 0}, // 36) exit if not protected filename
+        vm_inst{OP_LOAD, 0, 0, 0, 0}, // 35) set return val
+        vm_inst{OP_EXIT, 0, 0, 0, 0}, // 36) exit if not protected filename
     };
 };
