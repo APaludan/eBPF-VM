36863ed9839f585bb099152795b0d17d645e3016
andreas <andreashp@outlook.com>
2026-04-15 19:44:13 +0200

    added 2 kprobe programs

--- src/vm_instructions/vm_inst.cpp
+++ src/vm_instructions/vm_inst.cpp
@@ -4,37 +4,29 @@
 
 // Include the programs we want from programs folder
 #include "ptrace.h"
-#include "make_lsm.h"
+#include "bpf_lsm.h"
 #include "open_lsm.h"
-
-
+#include "pid_task.h"
+#include "find_vpid.h"
 
 std::unordered_map<int, std::vector<vm_inst>> generate_programs(pid_t protected_pid, bool with_junk)
 {
     std::unordered_map<int, std::vector<vm_inst>> program_map;
 
-    if (with_junk == false)
-    {
-        program_map[PTRACE_PROGRAM] = make_ptrace_program(protected_pid);
-        fix_jumps(program_map[PTRACE_PROGRAM]);
-        program_map[LSM_OPEN_PROGRAM] = make_lsm_open_program(protected_pid);
-        fix_jumps(program_map[LSM_OPEN_PROGRAM]);
-        program_map[LSM_BPF_PROGRAM] = make_lsm_bpf_program();
-        fix_jumps(program_map[LSM_BPF_PROGRAM]);
-    }
+    auto add_prog = [&](int id, std::vector<vm_inst>&& insts) {
+        if (with_junk) {
+            insts = generate_junk_inst(std::move(insts));
+        }
+        fix_jumps(insts);
+        program_map[id] = std::move(insts);
+    };
 
-    if (with_junk == true)
-    {
-        program_map[PTRACE_PROGRAM] = generate_junk_inst(make_ptrace_program(protected_pid));
-        fix_jumps(program_map[PTRACE_PROGRAM]);
-        program_map[LSM_OPEN_PROGRAM] = generate_junk_inst(make_lsm_open_program(protected_pid));
-        fix_jumps(program_map[LSM_OPEN_PROGRAM]);
-        program_map[LSM_BPF_PROGRAM] = generate_junk_inst(make_lsm_bpf_program());
-        fix_jumps(program_map[LSM_BPF_PROGRAM]);        
-    }
-
-
-    //print_program_map_to_csv(program_map, "program_map.csv");
+    // To add a new program, just add it here:
+    add_prog(PTRACE_PROGRAM,            make_ptrace_program(protected_pid));
+    add_prog(LSM_OPEN_PROGRAM,          make_lsm_open_program(protected_pid));
+    add_prog(LSM_BPF_PROGRAM,           make_lsm_bpf_program());
+    add_prog(KPROBE_FIND_VPID_PROGRAM,  make_kprobe_find_vpid_program(protected_pid));
+    add_prog(KPROBE_PID_TASK_PROGRAM,   make_kprobe_pid_task_program(protected_pid));
 
     return program_map;
 }
@@ -111,18 +103,22 @@ size_t inst_serialized_size(const vm_inst &inst)
 
 
 
-void fix_jumps(std::vector<vm_inst>& program) {
+void fix_jumps(std::vector<vm_inst> &program)
+{
     // sums of instruction sizes
     // offsets[i] = the byte offset of the instruction at program[i].
     std::vector<size_t> offsets(program.size() + 1, 0);
-    for (size_t i = 0; i < program.size(); i++) {
+    for (size_t i = 0; i < program.size(); i++)
+    {
         offsets[i + 1] = offsets[i] + inst_serialized_size(program[i]);
     }
 
-    for (size_t i = 0; i < program.size(); ++i) {
-        auto& inst = program[i];
-        
-        if (is_jump_op(inst.op)) {
+    for (size_t i = 0; i < program.size(); ++i)
+    {
+        auto &inst = program[i];
+
+        if (is_jump_op(inst.op))
+        {
             size_t target_idx = i + inst.val;
             inst.val = offsets[target_idx] - offsets[i];
         }
