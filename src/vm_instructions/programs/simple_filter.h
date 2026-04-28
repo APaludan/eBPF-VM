eeb36abe64864a833a4c0482c4debcfe114b562f
domroht <domroht@proton.me>
2026-04-28 13:22:33 +0200

    added const for simplefilter inst

--- src/vm_instructions/programs/simple_filter.h
+++ src/vm_instructions/programs/simple_filter.h
@@ -2,6 +2,11 @@
 
 std::vector<vm_inst> make_simple_filter_program()
 {
+    const auto ETH_P_IP = 0x0008;
+    const auto ETH_P_IPV6 = 0xDD86;
+    const unsigned int IPPROTO_ICMP = 1;
+    const unsigned int IPPROTO_ICMPV6 = 58;
+
     return {
         // struct xdp_md *ctx 
 
@@ -12,17 +17,17 @@ std::vector<vm_inst> make_simple_filter_program()
 
         // r2 = 0x0008 
         // (ETH_P_IP read as little-endian, so already converted, no need for bpf_htons)
-        vm_inst{OP_LOAD, 2, 0, 0x0008, 0},
+        vm_inst{OP_LOAD, 2, 0, ETH_P_IP, 0},
 
         // r3 = 0xDD86
         // same as before ETH_P_IPV6 = 0x86DD which is 0xDD86 as little-endian
-        vm_inst{OP_LOAD, 3, 0, 0xDD86, 0},
+        vm_inst{OP_LOAD, 3, 0, ETH_P_IPV6, 0},
 
         // So now we have eth-h_proto in register 1 and the two cases we want ot cheack 
         // case 1: bpf_htons(ETH_P_IP) = r2 and bpf_htons(ETH_P_IPV6) = r3
 
         //============================ case 1 ============================
-        // case 1: bpf_htons(ETH_P_IP) if r1 != r2 -> PASS
+        // case 1 (not ipv4): bpf_htons(ETH_P_IP) if r1 != r2 -> PASS
         vm_inst{OP_JNEQ, 1, 2, 8, 0},
 
         // r4 = ctx->protocol
@@ -34,7 +39,7 @@ std::vector<vm_inst> make_simple_filter_program()
         vm_inst{OP_READ_CTX, 4, 0, 1, 23},
 
         // r5 = 1 (IPPROTO_ICMP)
-        vm_inst{OP_LOAD, 5, 0, 1, 0},
+        vm_inst{OP_LOAD, 5, 0, IPPROTO_ICMP, 0},
 
         // if r1 != r2 -> PASS
         vm_inst{OP_JNEQ, 4, 5, 3, 0},
@@ -45,14 +50,14 @@ std::vector<vm_inst> make_simple_filter_program()
         vm_inst{OP_EXIT, 0, 0, 0, 0},
 
         //============================ case 2 ============================
-        // case 2: bpf_htons(ETH_P_IPV6) = r3 != r1 -> pass
+        // case 2 (not ipv6): bpf_htons(ETH_P_IPV6) = r3 != r1 -> pass
         vm_inst{OP_JNEQ, 1, 2, 8, 0},
 
         // r4 = ip6->nexthdr
         vm_inst{OP_READ_CTX, 4, 0, 1, 20},
 
         // r5 = 1 (IPPROTO_ICMP)
-        vm_inst{OP_LOAD, 5, 0, 58, 0},
+        vm_inst{OP_LOAD, 5, 0, IPPROTO_ICMPV6, 0},
 
         // if r1 != r2 -> PASS
         vm_inst{OP_JNEQ, 4, 5, 3, 0},
