e37a7be49ff84c5544db487667740ca15e624891
APaludan <andreashp@outlook.com>
2026-04-30 19:04:07 +0200

    Update ptrace.h

--- src/vm_instructions/programs/ptrace.h
+++ src/vm_instructions/programs/ptrace.h
@@ -9,7 +9,8 @@ std::vector<vm_inst> make_ptrace_program(pid_t protected_pid)
         vm_inst{OP_RSHIFT, 0, 0, 32, 0},               // 02) = r0 >> 32 (extract PID only)
         vm_inst{OP_READ_CTX, 2, 0, sizeof(pid_t), 24}, // 03) read the target pid from ctx + offset 24 = (ctx->args[1])
 
-        vm_inst{OP_JNEQ, 1, 2, 2, 0},    // 04) if r1(protected pid) != r2(target pid): jump to exit (pc +2)
+        vm_inst{OP_JNEQ, 1, 2, 3, 0},    // 04) if r1(protected pid) != r2(target pid): jump to exit (pc +3)
+        vm_inst{OP_JEQ, 1, 0, 2, 0},    // 04) if r1(protected pid) == r0(caller pid): jump to exit (pc +2)
         vm_inst{OP_RINGBUF, 0, 0, 0, 0}, // 05) submit info to ringbuf
         vm_inst{OP_LOAD, 0, 0, 0, 0},    // 06) set exit code
         vm_inst{OP_EXIT, 0, 0, 0, 0}     // 07) exit
