567b5aba38b3072c720cda146018960470236aec
APaludan <andreashp@outlook.com>
2026-05-20 10:31:53 +0200

    Update pid_task.h

--- src/vm_instructions/programs/pid_task.h
+++ src/vm_instructions/programs/pid_task.h
@@ -3,7 +3,7 @@
 std::vector<vm_inst> make_kprobe_pid_task_program()
 {
     return {
-        {OP_EXIT, 0, 0, 0, 0},
+        {OP_EXIT, 0, 0, 0, 0}, // to prevent spam in terminal
         {OP_READ_DATA, 1, 0, 0, 0},
         {OP_READ_CTX, 2, 0, sizeof(pid_t), 2768},
         {OP_JNEQ, 1, 2, 2, 0},
